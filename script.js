67459934796329192de001e391862e2082bdba39
demahesta <dewamahesta2711@gmail.com>
2026-08-02 22:31:10 +0800

    feat: modify music

--- script.js
+++ script.js
@@ -125,10 +125,10 @@ function createHeartShower() {
 function initAudio() {
     let isPlaying = false;
     let hasStartedOnce = false;
-    const START_TIME = 189; // Menit 3:09 = 189 detik
+    const START_TIME = 166; // Menit 2:46 = 166 detik
     const audio = new Audio("music/Nadhif Basalamah - penjaga hati.mp3");
 
-    // Pastikan pemutaran pertama dimulai dari menit 3:09 saat metadata lagu dimuat
+    // Pastikan pemutaran pertama dimulai dari menit 2:46 saat metadata lagu dimuat
     audio.addEventListener("loadedmetadata", () => {
         if (!hasStartedOnce) {
             audio.currentTime = START_TIME;
